5181930e82d0539073ed72c11c949cb110a92e7d
LDVSOFT <lapshin.dv@mail.ru>
2016-09-29 01:23:15 +0300

    Finished more ops!

--- src/Expr.ml
+++ src/Expr.ml
@@ -1,41 +1,27 @@
 module SS = Set.Make(String)
 
-type binop =
-  | Add
-  | Sub
-  | Mul
-  | Div
-  | Mod
-  | And
-  | Or
-  | Lt
-  | Gt
-  | Le
-  | Ge
-  | Eq
-  | Neq
-
 let eval_op op l r =
   let toi x = if x then 1 else 0 in
   match op with
-  | Add -> l + r
-  | Sub -> l - r
-  | Mul -> l * r
-  | Div -> l / r
-  | Mod -> l mod r
-  | And -> toi ((l != 0) && (r != 0))
-  | Or  -> toi ((l != 0) || (r != 0))
-  | Lt  -> toi (l < r)
-  | Gt  -> toi (l > r)
-  | Le  -> toi (l <= r)
-  | Ge  -> toi (l >= r)
-  | Eq  -> toi (l == r)
-  | Neq -> toi (l != r)
+  | "+" -> l + r
+  | "-" -> l - r
+  | "*" -> l * r
+  | "/" -> l / r
+  | "%" -> l mod r
+  | "&&" -> toi ((l != 0) && (r != 0))
+  | "||" -> toi ((l != 0) || (r != 0))
+  | "<"  -> toi (l < r)
+  | ">"  -> toi (l > r)
+  | "<=" -> toi (l <= r)
+  | ">=" -> toi (l >= r)
+  | "==" -> toi (l == r)
+  | "!=" -> toi (l != r)
+  | _ -> invalid_arg "Unknown op"
 
 type expr =
   | Const of int
   | Var   of string
-  | Op    of binop * expr * expr
+  | Op    of string * expr * expr
 
 let rec eval state expr =
   match expr with
@@ -85,7 +71,7 @@ type instr =
   | S_PUSH  of int
   | S_LD    of string
   | S_ST    of string
-  | S_OP    of binop
+  | S_OP    of string
 
 let srun input code =
   let rec srun' (state, stack, input, output) code =
@@ -177,21 +163,6 @@ type x86instr = (* src -> dest *)
   | X86Cdq
   | X86Call  of string
 
-let x86compile_op2 op l r = match op with
-  | Add -> X86Add (l, r)
-  | Sub -> X86Sub (l, r)
-  | Mul -> X86Mul (l, r)
-  | And -> X86And (l, r)
-  | Or  -> X86Or  (l, r)
-
-let x86compile_op1 op l = match op with
-  | Lt  -> X86Setl  l
-  | Le  -> X86Setle l
-  | Gt  -> X86Setg  l
-  | Ge  -> X86Setge l
-  | Eq  -> X86Sete  l
-  | Neq -> X86Setne l
-
 let x86compile : instr list -> x86instr list = fun code ->
   let x86addStack s =
     match s with
@@ -231,34 +202,111 @@ let x86compile : instr list -> x86instr list = fun code ->
         | _   -> (stack', [X86Mov (s, x86eax); X86Mov(x86eax, M x); X86Add (L word_size, x86esp)])
         in res
       | S_OP op -> (
+        let x86compile_op2 op l r = match op with
+          | "+"  -> X86Add (l, r)
+          | "-"  -> X86Sub (l, r)
+          | "*"  -> X86Mul (l, r)
+          | "&&" -> X86And (l, r)
+          | "||" -> X86Or  (l, r)
+          | _ -> invalid_arg ""
+        in
+        let x86compile_op1 op l = match op with
+          | "<"  -> X86Setl  l
+          | "<=" -> X86Setle l
+          | ">"  -> X86Setg  l
+          | ">=" -> X86Setge l
+          | "==" -> X86Sete  l
+          | "!=" -> X86Setne l
+          | _ -> invalid_arg ""
+        in
         let op_buf = x86eax in
         let re_buf = x86edx in
-        match op with
-        | Add | Sub | Mul | And | Or -> (
-          let y::x::stack' = stack in
-          let perform t = [x86compile_op2 op y t] in
-          match x with
-          | R _ -> (x::stack', perform x @ x86subStack y)
-          | _   -> (x::stack', [X86Mov (x, op_buf)] @ perform op_buf @ [X86Mov (op_buf, x)] @ x86subStack y)
-        )
-        | Lt | Le | Gt | Ge | Eq | Neq -> (
-          let y::x::stack' = stack in
-          let perform t = [X86Mov (L 0, re_buf); X86Cmp (y, t); x86compile_op1 op re_buf; X86Mov (re_buf, t)] in
-          match x with
-          | R _ -> (x::stack', perform x @ x86subStack y)
-          | _   -> (x::stack', [X86Mov (x, op_buf)] @ perform op_buf @ [X86Mov (op_buf, x)] @ x86subStack y)
-        )
-        | Div | Mod -> (
-          let y::x::stack' = stack in
-          let res = match op with
-            | Div -> x86eax
-            | Mod -> x86edx
-          in
-          (x::stack', [X86Mov (x, x86eax); X86Cdq; X86Div y; X86Mov (res, x)] @ x86subStack y)
-        )
+        let protect t t' f = match t with
+        | R x -> f t
+        | _ -> [X86Mov (t, t')] @ (f t') @ [X86Mov (t', t)]
+        in
+        let y::x::stack' = stack in (x::stack', (
+          match op with
+          | "+" | "-" | "*" | "&&" | "||" -> (
+            let checkBool t = match op with
+            | "&&" | "||" -> [X86Mov (t, x86eax); X86Mov (L 0, re_buf); X86Cmp (L 0, x86eax); X86Setne re_buf; X86Mov (re_buf, t)]
+            | _ -> []
+            in
+            checkBool x @ checkBool y @ protect x op_buf (fun x' -> [x86compile_op2 op y x'])
+          )
+          | "<" | "<=" | ">" | ">=" | "==" | "!=" -> (
+            protect x op_buf (fun x' -> [X86Mov (L 0, re_buf); X86Cmp (y, x'); x86compile_op1 op re_buf; X86Mov(re_buf, x')])
+          )
+          | "/" | "%" -> (
+            let res = match op with
+              | "/" -> x86eax
+              | "%" -> x86edx
+            in
+            [X86Mov (x, x86eax); X86Cdq; X86Div y; X86Mov (res, x)]
+          )
+        ) @ x86subStack y)
       )
    in
    x86code @ x86compile' stack' code'
   in
   (X86Mov (x86esp, x86ebp))::x86compile' [] code
+
+let print_code code b =
+  let rec pr_op opnd =
+    match opnd with
+    | R n -> x86regs.(n)
+    | S o -> Printf.sprintf "%d(%s)" ((-4) * o) (pr_op x86ebp)
+    | M s -> s
+    | L n -> Printf.sprintf "$%d" n
+  in
+  let rec pr_op8 opnd =
+    match opnd with
+    | R n -> x86regs8.(n)
+    | S o -> Printf.sprintf "%d(%s)" ((-4) * o) (pr_op x86ebp)
+    | M s -> s
+    | L n -> Printf.sprintf "$%d" n
+  in
+
+  Buffer.add_string b @@ Printf.sprintf "main:\n";
+  List.iter (fun instr ->
+    match instr with
+    | X86Add (o1, o2) -> Buffer.add_string b @@ Printf.sprintf "\tADDL \t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86Sub (o1, o2) -> Buffer.add_string b @@ Printf.sprintf "\tSUBL \t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86Mul (o1, o2) -> Buffer.add_string b @@ Printf.sprintf "\tIMULL\t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86And (o1, o2) -> Buffer.add_string b @@ Printf.sprintf "\tADDL \t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86Or  (o1, o2) -> Buffer.add_string b @@ Printf.sprintf "\tORL  \t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86Xor (o1, o2) -> Buffer.add_string b @@ Printf.sprintf "\tXORL \t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86Mov (o1, o2) -> Buffer.add_string b @@ Printf.sprintf "\tMOVL \t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86Cmp (o1, o2) -> Buffer.add_string b @@ Printf.sprintf "\tCMPL \t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86Div   o1     -> Buffer.add_string b @@ Printf.sprintf "\tIDIVL\t%s\n" (pr_op o1)
+    | X86Setl  o1     -> Buffer.add_string b @@ Printf.sprintf "\tSETL \t%s\n" (pr_op8 o1)
+    | X86Setle o1     -> Buffer.add_string b @@ Printf.sprintf "\tSETLE\t%s\n" (pr_op8 o1)
+    | X86Setg  o1     -> Buffer.add_string b @@ Printf.sprintf "\tSETG \t%s\n" (pr_op8 o1)
+    | X86Setge o1     -> Buffer.add_string b @@ Printf.sprintf "\tSETGE\t%s\n" (pr_op8 o1)
+    | X86Sete  o1     -> Buffer.add_string b @@ Printf.sprintf "\tSETE \t%s\n" (pr_op8 o1)
+    | X86Setne o1     -> Buffer.add_string b @@ Printf.sprintf "\tSETNE\t%s\n" (pr_op8 o1)
+    | X86Push o1      -> Buffer.add_string b @@ Printf.sprintf "\tPUSHL\t%s\n" (pr_op o1)
+    | X86Pop  o1      -> Buffer.add_string b @@ Printf.sprintf "\tPOPL \t%s\n" (pr_op o1)
+    | X86Ret          -> Buffer.add_string b @@ Printf.sprintf "\tRET  \n"
+    | X86Cdq          -> Buffer.add_string b @@ Printf.sprintf "\tCDQ  \n"
+    | X86Call s       -> Buffer.add_string b @@ Printf.sprintf "\tCALL \t%s\n" s
+  ) code;
+  Buffer.add_string b @@ Printf.sprintf "\tXORL\t%s,\t%s\n\tRET\n\n" (pr_op x86eax) (pr_op x86eax)
+
+let print_compiled: stmt -> string = fun stmt ->
+  let buffer = Buffer.create 1024 in
+  let asm = x86compile (compile_stmt stmt) in
+  let vars = collect_vars stmt in
+  Buffer.add_string buffer "\t.extern read\n\t.extern write\n\t.global main\n\n\t.text\n";
+  print_code asm buffer;
+  SS.iter (fun var ->
+    Buffer.add_string buffer @@ Printf.sprintf "\t.comm %s 4\n" var
+  ) vars;
+  Buffer.contents buffer
+
+let build: stmt -> string -> int = fun stmt file ->
+  let outf = open_out (Printf.sprintf "%s.s" file) in
+  Printf.fprintf outf "%s" (print_compiled stmt);
+  close_out outf;
+  Sys.command (Printf.sprintf "gcc -o %s runtime/runtime.o %s.s" file file)
 
