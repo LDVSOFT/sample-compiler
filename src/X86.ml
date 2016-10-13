25b039f8f656863dff826045b43e69518902fdc9
LDVSOFT <lapshin.dv@mail.ru>
2016-10-13 13:43:35 +0300

    Collapsed X86SetCC instrcution set to one type

--- src/X86.ml
+++ src/X86.ml
@@ -50,12 +50,7 @@ type x86instr = (* src -> dest *)
   | X86Mov   of opnd * opnd
   | X86Cmp   of opnd * opnd
   | X86Div   of opnd
-  | X86Setl  of opnd
-  | X86Setle of opnd
-  | X86Setg  of opnd
-  | X86Setge of opnd
-  | X86Sete  of opnd
-  | X86Setne of opnd
+  | X86SetCC of string * opnd
   | X86Push  of opnd
   | X86Pop   of opnd
   | X86Ret
@@ -122,12 +117,12 @@ let x86compile : i list -> x86instr list = fun code ->
           | _ -> invalid_arg ""
         in
         let x86compile_op1 op l = match op with
-          | "<"  -> X86Setl  l
-          | "<=" -> X86Setle l
-          | ">"  -> X86Setg  l
-          | ">=" -> X86Setge l
-          | "==" -> X86Sete  l
-          | "!=" -> X86Setne l
+          | "<"  -> X86SetCC ("L" , l)
+          | "<=" -> X86SetCC ("LE", l)
+          | ">"  -> X86SetCC ("G" , l)
+          | ">=" -> X86SetCC ("GE", l)
+          | "==" -> X86SetCC ("E" , l)
+          | "!=" -> X86SetCC ("NE", l)
           | _ -> invalid_arg ""
         in
         let protect t t' f = match t with
@@ -179,28 +174,23 @@ let print_code code b =
   Buffer.add_string b @@ Printf.sprintf "main:\n";
   List.iter (fun instr ->
     match instr with
-    | X86Add (o1, o2) -> Buffer.add_string b @@ Printf.sprintf "\tADDL \t%s,\t%s\n" (pr_op o1) (pr_op o2)
-    | X86Sub (o1, o2) -> Buffer.add_string b @@ Printf.sprintf "\tSUBL \t%s,\t%s\n" (pr_op o1) (pr_op o2)
-    | X86Mul (o1, o2) -> Buffer.add_string b @@ Printf.sprintf "\tIMULL\t%s,\t%s\n" (pr_op o1) (pr_op o2)
-    | X86And (o1, o2) -> Buffer.add_string b @@ Printf.sprintf "\tANDL \t%s,\t%s\n" (pr_op o1) (pr_op o2)
-    | X86Or  (o1, o2) -> Buffer.add_string b @@ Printf.sprintf "\tORL  \t%s,\t%s\n" (pr_op o1) (pr_op o2)
-    | X86Xor (o1, o2) -> Buffer.add_string b @@ Printf.sprintf "\tXORL \t%s,\t%s\n" (pr_op o1) (pr_op o2)
-    | X86Mov (o1, o2) -> Buffer.add_string b @@ Printf.sprintf "\tMOVL \t%s,\t%s\n" (pr_op o1) (pr_op o2)
-    | X86Cmp (o1, o2) -> Buffer.add_string b @@ Printf.sprintf "\tCMPL \t%s,\t%s\n" (pr_op o1) (pr_op o2)
-    | X86Div   o1     -> Buffer.add_string b @@ Printf.sprintf "\tIDIVL\t%s\n" (pr_op o1)
-    | X86Setl  o1     -> Buffer.add_string b @@ Printf.sprintf "\tSETL \t%s\n" (pr_op8 o1)
-    | X86Setle o1     -> Buffer.add_string b @@ Printf.sprintf "\tSETLE\t%s\n" (pr_op8 o1)
-    | X86Setg  o1     -> Buffer.add_string b @@ Printf.sprintf "\tSETG \t%s\n" (pr_op8 o1)
-    | X86Setge o1     -> Buffer.add_string b @@ Printf.sprintf "\tSETGE\t%s\n" (pr_op8 o1)
-    | X86Sete  o1     -> Buffer.add_string b @@ Printf.sprintf "\tSETE \t%s\n" (pr_op8 o1)
-    | X86Setne o1     -> Buffer.add_string b @@ Printf.sprintf "\tSETNE\t%s\n" (pr_op8 o1)
-    | X86Push o1      -> Buffer.add_string b @@ Printf.sprintf "\tPUSHL\t%s\n" (pr_op o1)
-    | X86Pop  o1      -> Buffer.add_string b @@ Printf.sprintf "\tPOPL \t%s\n" (pr_op o1)
-    | X86Ret          -> Buffer.add_string b @@ Printf.sprintf "\tRET  \n"
-    | X86Cdq          -> Buffer.add_string b @@ Printf.sprintf "\tCDQ  \n"
-    | X86Call s       -> Buffer.add_string b @@ Printf.sprintf "\tCALL \t%s\n" s
-    | X86Jmp  s       -> Buffer.add_string b @@ Printf.sprintf "\tJMP  \t%s\n" s
-    | X86Jne s        -> Buffer.add_string b @@ Printf.sprintf "\tJNE  \t%s\n" s
+    | X86Add (o1, o2)  -> Buffer.add_string b @@ Printf.sprintf "\tADDL \t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86Sub (o1, o2)  -> Buffer.add_string b @@ Printf.sprintf "\tSUBL \t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86Mul (o1, o2)  -> Buffer.add_string b @@ Printf.sprintf "\tIMULL\t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86And (o1, o2)  -> Buffer.add_string b @@ Printf.sprintf "\tANDL \t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86Or  (o1, o2)  -> Buffer.add_string b @@ Printf.sprintf "\tORL  \t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86Xor (o1, o2)  -> Buffer.add_string b @@ Printf.sprintf "\tXORL \t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86Mov (o1, o2)  -> Buffer.add_string b @@ Printf.sprintf "\tMOVL \t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86Cmp (o1, o2)  -> Buffer.add_string b @@ Printf.sprintf "\tCMPL \t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86Div   o1      -> Buffer.add_string b @@ Printf.sprintf "\tIDIVL\t%s\n" (pr_op o1)
+    | X86SetCC (s, o1) -> Buffer.add_string b @@ Printf.sprintf "\tSET%s\t%s\n" s (pr_op8 o1)
+    | X86Push o1       -> Buffer.add_string b @@ Printf.sprintf "\tPUSHL\t%s\n" (pr_op o1)
+    | X86Pop  o1       -> Buffer.add_string b @@ Printf.sprintf "\tPOPL \t%s\n" (pr_op o1)
+    | X86Ret           -> Buffer.add_string b @@ Printf.sprintf "\tRET  \n"
+    | X86Cdq           -> Buffer.add_string b @@ Printf.sprintf "\tCDQ  \n"
+    | X86Call s        -> Buffer.add_string b @@ Printf.sprintf "\tCALL \t%s\n" s
+    | X86Jmp  s        -> Buffer.add_string b @@ Printf.sprintf "\tJMP  \t%s\n" s
+    | X86Jne s         -> Buffer.add_string b @@ Printf.sprintf "\tJNE  \t%s\n" s
     | X86_Label s -> Buffer.add_string b @@ Printf.sprintf "%s:\n" s
   ) code
 
